2ecb0878e9f94a185fe10d4b9a28febf7533cad6
septchn <107803717+septchn@users.noreply.github.com>
2022-06-20 00:42:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,16 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#====================================================================
+# 添加插件源码
+sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git-full kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+sed -i '$a src-git-full Boos4721 https://github.com/Boos4721/OpenWrt-Packages' feeds.conf.default
+
+# Argon_dark_purple
+sed -i '$a src-git-full liuran001 https://github.com/liuran001/openwrt-packages' feeds.conf.default
+
+# passwall依赖
+sed -i '$a src-git-full small https://github.com/kenzok8/small' feeds.conf.default
+#====================================================================
